316e5ef2625da476d479f1ddbdf9728885cb74f9
Mirt12 <Mirt12@yandex.ru>
2023-05-29 14:10:25 +0200

    add UpperCase print tool, add Random print tool to Main class

--- src/com/oefening/Main.java
+++ src/com/oefening/Main.java
@@ -11,5 +11,9 @@ public class Main {
         int j = (int) Math.floor(Math.random() * str.length());
         ch = str.charAt(j);
         System.out.println(ch);
+        //to print char in UpperCase:
+        ch = 'q';
+        char chU = Character.toUpperCase(ch);
+        System.out.println(chU);
     }
 }
